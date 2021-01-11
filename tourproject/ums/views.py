4bb70b39b728fd530bb09efce0e95774b00ff682
Shishir <shishirs520@gmail.com>
2021-01-11 14:36:54 +0545

    restricted access to register and login page if user is authenticated

--- tourproject/ums/views.py
+++ tourproject/ums/views.py
@@ -25,20 +25,23 @@ def register_page(request):
     
 
 def login_page(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
 
-        user = authenticate(request, username=username, password=password)
-        
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            messages.info(request, 'Username or Password is incorrect')
+            user = authenticate(request, username=username, password=password)
+            
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, 'Username or Password is incorrect')
 
-    context = {}
-    return render(request, 'ums/login.html', context)
+        context = {}
+        return render(request, 'ums/login.html', context)
 
 
 def logout_user(request):
