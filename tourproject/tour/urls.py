3d8fa277d70a8ea05c6d3a98ff68fe5db7404cd8
Shishir <shishirs520@gmail.com>
2021-03-16 23:30:58 +0545

    added update operation (CRUD done)

--- tourproject/tour/urls.py
+++ tourproject/tour/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('delete_destination/<int:id>/', views.deleteDestination, name='delete_destination'),
     path('delete_hotel/<int:id>/', views.deleteHotel, name='delete_hotel'),
     path('delete_airline/<int:id>/', views.deleteAirline, name='delete_airline'),
+    path('update_destination/<int:id>/', views.updateDestination, name='update_destination'),
+    path('update_hotel/<int:id>/', views.updateHotel, name='update_hotel'),
+    path('update_airline/<int:id>/', views.updateAirline, name='update_airline'),
 ]
--- tourproject/tour/views.py
+++ tourproject/tour/views.py
@@ -73,3 +73,45 @@ def deleteAirline(request, id):
         return redirect('airlines')
     context = {'airline': airline}
     return render(request, 'tour/airlines.html', context)
+
+
+@login_required(login_url='login')
+def updateDestination(request, id):
+    allDestinations = Destination.objects.all()
+    destination = Destination.objects.get(id=id)
+    form = DestinationForm(instance=destination)
+    if request.method == 'POST':
+        form = DestinationForm(request.POST, instance=destination)
+        if form.is_valid():
+            form.save()
+            return redirect('destinations')
+    context = {'allDestinations': allDestinations, 'form': form}
+    return render(request, 'tour/destinations.html', context)
+    
+
+@login_required(login_url='login')
+def updateHotel(request, id):
+    allHotels = Hotel.objects.all()
+    hotel = Hotel.objects.get(id=id)
+    form = HotelForm(instance=hotel)
+    if request.method == 'POST':
+        form = HotelForm(request.POST, instance=hotel)
+        if form.is_valid():
+            form.save()
+            return redirect('hotels')
+    context = {'allHotels': allHotels, 'form': form}
+    return render(request, 'tour/hotels.html', context)
+    
+
+@login_required(login_url='login')
+def updateAirline(request, id):
+    allAirlines = Airline.objects.all()
+    airline = Airline.objects.get(id=id)
+    form = AirlineForm(instance=airline)
+    if request.method == 'POST':
+        form = AirlineForm(request.POST, instance=airline)
+        if form.is_valid():
+            form.save()
+            return redirect('airlines')
+    context = {'allAirlines': allAirlines, 'form': form}
+    return render(request, 'tour/airlines.html', context)
