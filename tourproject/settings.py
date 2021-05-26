c22b57bfffe7b742b6731fe5d0a657e9b1519f79
Shishir <shishirs520@gmail.com>
2021-05-26 15:09:17 +0545

    updated STATIC_ROOT

--- tourproject/settings.py
+++ tourproject/settings.py
@@ -120,8 +120,5 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
