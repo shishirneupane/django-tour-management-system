3334dd1687810c113dd21338b82166f788e8311a
Shishir <shishirs520@gmail.com>
2021-05-26 17:59:13 +0545

    staticfiles in settings.py

--- tourproject/settings.py
+++ tourproject/settings.py
@@ -124,6 +124,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = BASE_DIR / 'static'
 STATIC_URL = '/static/'
 
 
